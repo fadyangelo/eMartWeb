b7db2678c421bf18e5f92f444536396cb3acbaf9
fadyangelo <fady.angelo@itspark-eg.com>
2026-06-27 19:05:36 +0300

    Commit changes from Laptop

--- 03_order_itspark-eg_nodejs.bat
+++
@@ -1,7 +0,0 @@
-cd C:\Fady\root\order.itspark-eg.com\website-calculator_users
-yarn dev --port 4000
-
-pause
-
-
-
---
+++ 09_emart2_itspark-eg_nodejs.bat
@@ -0,0 +1,7 @@
+C:\Fady\root\emart2.itspark-eg.com\eMartWeb
+yarn dev --port 4078
+
+pause
+
+
+
